e84e7781986d1e48f26725ff09da2dbfc02653a8
wajmar <hammami.ouajdi@gmail.com>
2016-12-15 12:41:20 +0100

    model

---
+++ TypeTpl.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Request;
+use Location;
+use Laralum;
+
+class TypeTpl extends Model
+{
+    protected $table = 'typetpl';  
+
+}
